58d9be188e38f7a9f10c34b2555f261a370d4d83
Jayson Zhu <jzhu@zenefits.com>
2017-05-16 17:02:19 -0700

    updating department rules

--- parse/parse.py
+++ parse/parse.py
@@ -6,21 +6,132 @@ RULE_TITLE = "title"
 RULE_CONTENT = "content"
 RULE_SUBRULE = "subsections"
 
-def ruleTemplate(content, options, conditionString, name, description, parentIndex=None, rootNode=False):
-	if rootNode:
-		condition = [
+
+def generalDepartRule(rules):
+	condition = [
+		"and",
+		[
 			"eq",
-			"@visitor_requesting_chat",
-			True
+			"@message",
+			"agent"
+		],
+		[
+			"eq",
+			"@sender_type",
+			"visitor"
+		]
+	]
+	for rule in rules:
+		condition.append([
+			"not",
+			[
+				"hasTag",
+				rule['tag']
+			]
+		])
+
+	return [{
+		"definition": {
+			"event": "chat_message",
+			"condition": condition,
+			"actions": [
+				[
+					"setDepartment",
+					1817723131
+				],
+				[
+					"sendMessageToVisitor",
+					"Customer Service",
+					"Transfeting you to General Inquiry Department"
+				]
+			]
+		},
+		"enabled": True,
+		"description": "Rule for human contact without tags",
+		"name": "generalDepartRule"
+	}]
+
+def parseDepartmentRule(rule):
+	return [{
+		"definition": {
+			"event": "chat_message",
+			"condition": [
+				"and",
+				[
+					"eq",
+					"@message",
+					"agent"
+				],
+				[
+					"eq",
+					"@sender_type",
+					"visitor"
+				],
+				[
+					"hasTag",
+					rule['tag']
+				]
+			],
+			"actions": [
+				[
+					"setDepartment",
+					rule['departmentId']
+				],
+				[
+					"sendMessageToVisitor",
+					"Customer Service",
+					"Transfeting you to {} Department".format(rule['departmentName'])
+				]
+			]
+		},
+		"enabled": True,
+		"description": "Assign employee to agent in department {} due to tag {}".format(rule['departmentName'], rule['tag']),
+		"name": "agent_tag_{}_department_{}".format(rule['tag'], rule['departmentName'])
+	}]
+
+def ruleTemplate(content, options, conditionString, name, description, tags=[], parentIndex=None, rootNode=False):
+	if rootNode:
+		event = 'chat_requested'
+		condition = [
+			"and",
+			[
+				"or",
+				[
+					"eq",
+					"@visitor_requesting_chat",
+					True
+				]
+			],
+			[
+				"not",
+				[
+					"firedBefore"
+				]
+			],
+			[
+				"eq",
+				"@sender_type",
+				"visitor"
+			]
 		]
 
 	else:
+		event = 'chat_message'
 		condition = [
-			"icontains",
-			"@message",
-			conditionString
+			"and",
+			[
+				"eq",
+				"@message",
+				conditionString
+			],
+			[
+				"eq",
+				"@sender_type",
+				"visitor"
+			]
 		]
 
+	actions = []
 	if options:
 		optionsWithIndex = []
 		for i in xrange(len(options)):
@@ -42,23 +153,28 @@ def ruleTemplate(content, options, conditionString, name, description, parentInd
 			content,
 			"/".join(optionsWithIndex)
 		]
+		actions.append(action)
 	else:
 		action = [
 			"sendMessageToVisitor",
 			"Customer Service",
 			content
 		]
+		actions.append(action)
+
+
+	if tags:
+		for tag in tags:
+			actions.append([
+				"addTag",
+				tag
+			])
 
 	return {
 		"definition": {
-			"actions": [
-				action
-			],
-			"event": "chat_message",
-			"condition": [
-				"or",
-				condition
-			]
+			"actions": actions,
+			"event": event,
+			"condition": condition
 		},
 		"enabled": True,
 		"description": description,
@@ -76,13 +192,14 @@ def parseRule(rule, parentRule, parentRuleIndex):
 	content = rule[RULE_CONTENT]
 	options = [_getRuleKey(subrule) for subrule in rule[RULE_SUBRULE]] if RULE_SUBRULE in rule else []
 	conditionString = _getRuleKey(rule) if not parentRuleIndex else "({}) {}".format(parentRuleIndex, _getRuleKey(rule))
-	name = _getRuleKey(rule)
 	description = rule[RULE_CONTENT]
 	parsedRules = []
 	if not parentRuleIndex:
-		parsedRules.append(ruleTemplate(content, options, conditionString, name, description, parentIndex=parentRuleIndex, rootNode=True))
+		name = rule[RULE_TITLE]
+		parsedRules.append(ruleTemplate(content, options, conditionString, name, description, rule.get('tags', []), parentIndex=parentRuleIndex, rootNode=True))
 
-	parsedRules.append(ruleTemplate(content, options, conditionString, name, description, parentIndex=parentRuleIndex))
+	name = _getRuleKey(rule)
+	parsedRules.append(ruleTemplate(content, options, conditionString, name, description, rule.get('tags', []), parentIndex=parentRuleIndex))
 	for i in xrange(len(rule.get(RULE_SUBRULE, []))):
 		subRule = rule[RULE_SUBRULE][i]
 		parsedRules += parseRule(subRule, rule, "{}.{}".format(parentRuleIndex, i + 1) if parentRuleIndex else "{}".format(i + 1))
@@ -95,8 +212,15 @@ def parseRuleFile(filename='rule.json'):
 
 	with open(filename) as json_data:
 		rules = json.load(json_data)
-		for rule in rules:
+		msgRule = rules['messageRules']
+		for rule in msgRule:
 			finalRules += parseRule(rule, None, None)
+
+		departmentRules = rules['departmentRules']
+		for rule in departmentRules:
+			finalRules += parseDepartmentRule(rule)
+
+		finalRules += generalDepartRule(departmentRules)
 
 	return finalRules
 
@@ -111,7 +235,6 @@ def submit(newTriggers):
 	for t in oldTriggers:
 		print 'deleting', t['name']
 		r = requests.delete('https://www.zopim.com/api/v2/triggers/{}'.format(t['name']), headers=headers)
-		print r
 
 	for t in newTriggers:
 		print 'adding', t
@@ -119,6 +242,5 @@ def submit(newTriggers):
 		print r
 
 rules = parseRuleFile()
-for rule in rules:
-	print rule['definition']['condition'][1][2]
-# submit(rules)
+print rules
+submit(rules)
